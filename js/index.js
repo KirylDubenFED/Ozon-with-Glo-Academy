d54bb5a6e33c0cccc3a65c86e86895c8a34caf7a
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-12-18 02:51:29 +0300

    add modal

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log('dfsfs');
+const cartBtn = document.getElementById('cart');
+const cartModal = document.querySelector('.cart')
+const cartCloseBtn = cartModal.querySelector('.cart-close')
+
+const openCart = () => {
+    cartModal.style.display = 'flex'
+}
+
+const closeCart = () => {
+    cartModal.style.display = ''
+}
+
+cartBtn.addEventListener('click', openCart)
+
+
+
+cartCloseBtn.onclick = closeCart
+
+console.log(cartBtn);

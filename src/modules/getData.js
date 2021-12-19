6fd6c3f04ed910492cdb949ce79be2880f23c786
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-12-19 23:46:20 +0300

    add getData

---
+++ src/modules/getData.js
@@ -0,0 +1,5 @@
+const getData = () => {
+return [1, 2, 3]
+}
+
+export default getData
--- src/modules/second.js
+++ src/modules/second.js
@@ -1,7 +1,9 @@
+import getData from './getData'
+
 const second = () => {
   const cartBtn = document.getElementById('cart')
 
-  console.log(cartBtn);
+  console.log(getData());
 }
 
 export default second
